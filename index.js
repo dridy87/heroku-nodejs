2c6c42a3c09e3c6e1d36784607c1a64b01ad13a2
dridy87@gmail.com <dridy@naver.com>
2020-12-26 03:23:08 +0900

    test

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express'),
+    path = require('path'),
+    bodyParser = require('body-parser'),
+    cors = require('cors'),
 
-const app = express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-
+const app = express();
 app.use(bodyParser.json());
 app.use(cors());
+const PORT = process.env.PORT
+//const port = process.env.PORT || 4000;
 
-
-app.get("/api/hello", (req, res) => {
-  res.send({ hello: "world" });
-  });
+app.get("/", (req, res) => {
+    res.send({ hello: "world" });
+    });
+const server = app.listen(port, function () {
+    console.log('Listening on port ' + port);
+});
