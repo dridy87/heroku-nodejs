6415b0197c8fc1f64b32ef08da6771409e8d9911
dridy87@gmail.com <dridy@naver.com>
2020-12-26 03:29:02 +0900

    npm u

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const express = require('express'),
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
-const PORT = process.env.PORT
-//const port = process.env.PORT || 4000;
+//const PORT = process.env.PORT
+const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
     res.send({ hello: "world" });
